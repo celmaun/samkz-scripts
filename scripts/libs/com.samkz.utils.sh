17036e1eb5df54b1d590f5453f2142fd3d3e83e2
salmatron <salman@osalman.com>
2023-11-19 04:53:56 +0500

    Add LOCAL__ETC env var ~/.config

--- scripts/libs/com.samkz.utils.sh
+++ scripts/libs/com.samkz.utils.sh
@@ -23,12 +23,27 @@ is_shell_program() { [ -f "${1-}" ] && [ -x "$1" ] || return; case "$(orex file 
 file_user() { [ -e "$1" ] && (x="$(command ls -ld "$1")" || exit; set -f -- ${x:?} || exit; user="${3-}"; id -urn "${user:?}"); }
 file_group() { [ -e "$1" ] && (x="$(command ls -ld "$1")" || exit; set -f -- ${x:?} || exit; group="${4-}"; printf '%s\n' "${group:?}"); }
 file_user_colon_group() { [ -e "$1" ] && (x="$(command ls -ld "$1")" || exit; set -f -- ${x:?} || exit; user="${3-}"; group="${4-}"; printf '%s:%s\n' "$(id -urn "${user:?}")" "${group:?}"); }
-    
+
+local_user_owner () {  
+  [ "$(id -ur)" -eq 0 ] || return 0
+  
+  [ -e "$1" ] || return
+  
+  x="$(orex readlink -f -- "$1")" || return
+  
+  if [ -d "$x" ]; then
+      oret chown -R "${LOCAL__USER:?}:" "$x" || return
+      return
+  fi
+  
+  oret chown "${LOCAL__USER:?}:" "$x" || return
+}
+
 print__LOCAL__USER() {
     set +e -u
 
     u=
-    while :; do
+    for _ in _; do
       u="$(id -urn)"; [ -z "${u##root}" ] || break
       u="$(id -un)"; [ -z "${u##root}" ] || break
       u="$(logname)"; [ -z "${u##root}" ] || break
@@ -38,8 +53,11 @@ print__LOCAL__USER() {
 
     [ -n "${u##root}" ] && printf '%s\n' "${u:?}" && exit
 
-    set -- "${BASH_SOURCE-}" "${0-}" "${HOME-}" "$PWD"
-    for f; do u="$(file_user "$f")" && [ -n "${u##root}" ] && id -urn "${u:?}" && exit; done
+    # set -- "${BASH_SOURCE-}" "${0-}" "${HOME-}" "$PWD"
+
+    for f in "${BASH_SOURCE-}" "${0-}" "${HOME-}" "$PWD"; do 
+      u="$(file_user "$f")" && [ -n "${u##root}" ] && id -urn "${u:?}" && exit
+    done
 
     exit "1$(>&2 printf '%s\n' "Unable to find non-root user")"
 }
@@ -54,7 +72,9 @@ export__LOCAL__USER() {
     LOCAL__GID="$(id -gr "${LOCAL__USER:?}")"
     LOCAL__EGID="$(id -g "${LOCAL__USER:?}")"
     eval "LOCAL__HOME=~${LOCAL__USER:?}"; export LOCAL__HOME
+    LOCAL__ETC="${LOCAL__HOME:?}/.config"
     set +a
+    [ -d "$LOCAL__ETC" ] || { mkdir "$LOCAL__ETC"; [ "$(id -ur)" -gt 0 ] || chown "$LOCAL__USER:" "$LOCAL__ETC"; }
     
     ${LOCAL__BIN:+:} export__LOCAL__BIN
 }
